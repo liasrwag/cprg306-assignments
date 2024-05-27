51931a3bef7844ee6c06e3ca53199203051c9143
Heru <Heru.Wang@edu.sait.ca>
2024-05-27 13:27:21 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import React from 'react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2">Week 2 Assignments</Link>
+    </div>
   );
 }
