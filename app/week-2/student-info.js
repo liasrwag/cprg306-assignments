51543525325cb5d1f48c638c75612d1cdcf00e33
Heru <Heru.Wang@edu.sait.ca>
2024-05-27 13:27:26 +0800

    Create student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Heru Wang</h1>
+      <Link href="https://github.com/liasrwag/cprg306-assignments">My GitHub Repository</Link>
+    </div>
+  );
+}
+
+export default StudentInfo;
