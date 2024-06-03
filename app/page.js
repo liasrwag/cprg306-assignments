5231c4a0ace2afce77d2edc811067096c2680b68
Heru <Heru.Wang@edu.sait.ca>
2024-06-04 04:17:11 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,6 +6,9 @@ export default function Home() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">Week 2 Assignments</Link>
+      <Link href="/week-3/page">Week 3 Assignments</Link>
+      <Link href="/week-4/functions">Week 4 Assignments</Link>
+
     </div>
   );
 }
