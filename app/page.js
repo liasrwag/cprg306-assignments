5c7786e2140b52fef87ad43b22ff195a84296acc
Heru <Heru.Wang@edu.sait.ca>
2024-06-17 15:17:21 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,11 +3,17 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignments</Link>
-      <Link href="/week-3/page">Week 3 Assignments</Link>
-      <Link href="/week-4/functions">Week 4 Assignments</Link>
+    <div className='h-screen p-6'>
+      <h1 className='mb-4'>CPRG 306: Web Development 2 - Assignments</h1>
+
+      <div className='mb-2'>      
+      <Link href="/week-2">Week 2 Assignments</Link></div>
+
+      <div className='mb-2'>      
+      <Link href="/week-3">Week 3 Assignments</Link></div>
+
+      <div className='mb-2'>      
+      <Link href="/week-4">Week 4 Assignments</Link></div>
 
     </div>
   );
